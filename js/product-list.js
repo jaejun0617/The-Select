a4caf74d76ecd9eb3747e39c2ace399004747a5b
ShinJaeJun <120928080+jaejun0617@users.noreply.github.com>
2025-08-19 14:40:35 +0900

    Update product-list.js

--- js/product-list.js
+++ js/product-list.js
@@ -1,9 +1,9 @@
 // product-list.js
 
 // utils.js 파일에서 fetchProducts와 formatCurrency 함수를 가져옵니다.
-import { fetchProducts, formatCurrency } from './utils.js';
+import { fetchProducts, formatCurrency } from '../utils.js';
 // modal.js 파일에서 필요한 모달 관련 함수들을 가져옵니다.
-import { openModal, renderModalContent, initializeModal } from './modal.js';
+import { openModal, renderModalContent, initializeModal } from '../modal.js';
 
 document.addEventListener('DOMContentLoaded', function () {
    // --- 1. STATE MANAGEMENT ---
